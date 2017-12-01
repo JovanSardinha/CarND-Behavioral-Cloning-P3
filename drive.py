3779b81f087c978fffbb8f09d7df748afcf232bf
Jovan Sardinha <jovan.sardinha@gmail.com>
2017-12-01 15:28:02 -0500

    changes to drive speed = 10

--- drive.py
+++ drive.py
@@ -44,7 +44,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 10
 controller.set_desired(set_speed)
 
 
@@ -137,5 +137,3 @@ if __name__ == '__main__':
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
-
-
