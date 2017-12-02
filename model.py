a14dc3f4f5494a9eefce2b5d64a3beed864474d3
Jovan Sardinha <jovan.sardinha@gmail.com>
2017-12-01 19:42:02 -0500

    updates to variables in model.py

--- model.py
+++ model.py
@@ -29,8 +29,8 @@ hyperparams = {}
 hyperparams['STEERING_CORRECTION'] = 0.2
 hyperparams['ADD_FLIPS'] = True
 hyperparams['ADD_SIDE_VIEWS'] = True
-hyperparams['KEEP_ZERO'] = False
-hyperparams['KEEP_ZERO_STEERING_ANGLE_THRESHOLD'] = 0.4
+hyperparams['KEEP_0'] = False
+hyperparams['KEEP_0_ANGLE_THRESHOLD'] = 0.4
 hyperparams['NORMALIZE_BRIGHTNESS'] = False
 hyperparams['SEED_VAL'] = 13
 
@@ -40,20 +40,17 @@ runs = ""
 
 def normalize_brightness(img):
     img_yuv = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    # equalize the histogram of the Y channel
     img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-    # convert the YUV image back to RGB format
     return cv2.cvtColor(img_yuv, cv2.COLOR_YUV2RGB)
 
 
-if  hyperparams['KEEP_ZERO'] == True:
+if  hyperparams['KEEP_0'] == True:
     temp = []
     np.random.seed(SEED_VAL)
-    # Skip 0 deg steering angle images
     for line in lines:
         if float(line[3].strip()) != 0.0:
             temp.append(line)
-        elif np.random.uniform() > hyperparams['KEEP_ZERO_STEERING_ANGLE_THRESHOLD']:
+        elif np.random.uniform() > hyperparams['KEEP_0_ANGLE_THRESHOLD']:
             temp.append(line)
     lines = temp
 
@@ -112,20 +109,20 @@ def generator(samples, batch_size=BATCH_SIZE):
 def NVIDIA_model(input_shape):
     inputs = Input(shape=input_shape)
 
-    x = Lambda(lambda img: (img / 255.0) - 0.5)(inputs)
-    x = Cropping2D(cropping=((70, 25), (0, 0)))(x)
-    x = Conv2D(24, (5, 5), strides=(2, 2), activation='relu', name='conv1')(inputs)
-    x = Conv2D(36, (5, 5), strides=(2, 2), activation='relu', name='conv2')(x)
-    x = Conv2D(48, (5, 5), strides=(2, 2), activation='relu', name='conv3')(x)
-    x = Conv2D(64, (3, 3), activation='relu', name='conv4')(x)
-    x = Conv2D(64, (3, 3), activation='relu', name='conv5')(x)
+    l_0 = Lambda(lambda img: (img / 255.0) - 0.5)(inputs)
+    l_0 = Cropping2D(cropping=((70, 25), (0, 0)))(l_0)
+    l_1  = Conv2D(24, (5, 5), strides=(2, 2), activation='relu', name='conv1')(l_0)
+    l_1 = Conv2D(36, (5, 5), strides=(2, 2), activation='relu', name='conv2')(l_1)
+    l_1 = Conv2D(48, (5, 5), strides=(2, 2), activation='relu', name='conv3')(l_1)
+    l_1 = Conv2D(64, (3, 3), activation='relu', name='conv4')(l_1)
+    l_1 = Conv2D(64, (3, 3), activation='relu', name='conv5')(l_1)
 
-    x = Flatten(name='flatten')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(100, activation='relu', name='fc1')(x)
-    x = Dense(50, activation='relu', name='fc2')(x)
-    x = Dense(10, activation='relu', name='fc3')(x)
-    output = Dense(1, name='output_layer')(x)
+    l_2 = Flatten(name='flatten')(l_1)
+    l_2 = Dropout(0.5)(x)
+    l_2 = Dense(100, activation='relu', name='fc1')(l_2)
+    l_2 = Dense(50, activation='relu', name='fc2')(l_2)
+    l_2 = Dense(10, activation='relu', name='fc3')(l_2)
+    output = Dense(1, name='output_layer')(l_2)
 
     model = Model(inputs=inputs, outputs=output)
     return model
@@ -137,51 +134,51 @@ print('Train_samples Shape', len(train_samples))
 print('Validation_samples Shape', len(validation_samples))
 
 
+if __name__ == '__main__':
+    # Training new model
+    ts = str(int(time.time()))
+    model_name = 'nvidia'
+    num_epochs = 50
+    steps_per_epoch = int(len(train_samples)/BATCH_SIZE)
+    run_name = 'model={}-batch_size={}-num_epoch={}-steps_per_epoch={}-run-{}-ts={}'.format(model_name,
+                                                                              BATCH_SIZE,
+                                                                              num_epochs,
+                                                                              steps_per_epoch,
+                                                                              runs,
+                                                                              ts)
+    print('run name:', run_name)
+    tensorboard_loc = os.path.join(TENSORBOARD_PATH, run_name)
+    checkpoint_loc = os.path.join(MODELS_PATH, 'model-{}.h5'.format(ts))
 
-# Training new model
-ts = str(int(time.time()))
-model_name = 'nvidia'
-num_epochs = 50
-steps_per_epoch = int(len(train_samples)/BATCH_SIZE)
-run_name = 'model={}-batch_size={}-num_epoch={}-steps_per_epoch={}-run-{}-ts={}'.format(model_name,
-                                                                          BATCH_SIZE,
-                                                                          num_epochs,
-                                                                          steps_per_epoch,
-                                                                          runs,
-                                                                          ts)
-print('run name:', run_name)
-tensorboard_loc = os.path.join(TENSORBOARD_PATH, run_name)
-checkpoint_loc = os.path.join(MODELS_PATH, 'model-{}.h5'.format(ts))
+    earlyStopping = EarlyStopping(monitor='val_loss',
+                                  patience=3,
+                                  verbose=1,
+                                  min_delta = 1e-4,
+                                  mode='min')
 
-earlyStopping = EarlyStopping(monitor='val_loss',
-                              patience=3,
-                              verbose=1,
-                              min_delta = 1e-4,
-                              mode='min')
+    modelCheckpoint = ModelCheckpoint(checkpoint_loc,
+                                      monitor = 'val_loss',
+                                      save_best_only = True,
+                                      mode = 'min',
+                                      verbose = 1)
 
-modelCheckpoint = ModelCheckpoint(checkpoint_loc,
-                                  monitor = 'val_loss',
-                                  save_best_only = True,
-                                  mode = 'min',
-                                  verbose = 1)
+    tensorboard = TensorBoard(log_dir=tensorboard_loc, histogram_freq=0, write_graph=True, write_images=True)
+    callbacks_list = [modelCheckpoint, earlyStopping, tensorboard]
 
-tensorboard = TensorBoard(log_dir=tensorboard_loc, histogram_freq=0, write_graph=True, write_images=True)
-callbacks_list = [modelCheckpoint, earlyStopping, tensorboard]
+    train_generator = generator(train_samples)
+    validation_generator = generator(validation_samples)
 
-train_generator = generator(train_samples)
-validation_generator = generator(validation_samples)
+    model = NVIDIA_model(IMG_SHAPE)
+    optim = Adam(lr=1e-4)
+    model.compile(loss='mse', optimizer=optim)
+    print(model.summary())
 
-model = NVIDIA_model(IMG_SHAPE)
-optim = Adam(lr=1e-4)
-model.compile(loss='mse', optimizer=optim)
-print(model.summary())
+    model.fit_generator(train_generator,
+                        steps_per_epoch=steps_per_epoch,
+                        validation_data=validation_generator,
+                        validation_steps=len(validation_samples),
+                        verbose=1,
+                        callbacks=callbacks_list,
+                        epochs=num_epochs)
 
-model.fit_generator(train_generator,
-                    steps_per_epoch=steps_per_epoch,
-                    validation_data=validation_generator,
-                    validation_steps=len(validation_samples),
-                    verbose=1,
-                    callbacks=callbacks_list,
-                    epochs=num_epochs)
-
-print('Model traning complete: {}'.format(run_name))
+    print('Model traning complete: {}'.format(run_name))
